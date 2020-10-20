7effccadcf175a61e15652ef8e41a6f4469b7a52
Alisher.Kenzhebayev <alisher.kenzhebayev@nu.edu.kz>
2020-10-20 22:46:56 +0600

    Add .gitignore. Initial commit.

---
+++ 07_Fundamentals/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace U07_Fundamentals
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+
+        public string ContactPhone { get; set; }
+
+        public string Revenue { get; set; }
+
+    }
+}
