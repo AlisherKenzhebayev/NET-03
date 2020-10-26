409fb3697784f587e6139fdeaa8c99d113214959
Alisher_K <avrora21262@gmail.com>
2020-10-26 18:02:39 +0600

    Add broken U10.T_01

---
+++ Source/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Source
+{
+    public static class BinarySearchHelper
+    {
+        // Should return an Enumerator to the generic collection of T
+        public static IEnumerator<T> BinarySearch<T>(List<T> collection, T search, IEnumerator<T> left, IEnumerator<T> right)
+        {
+            IEnumerator<T> middle = right + left;
+            if(collection[])
+            
+            return collection.GetEnumerator();
+        }
+    }
+}
