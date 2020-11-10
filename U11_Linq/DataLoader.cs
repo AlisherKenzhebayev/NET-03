e88a93e9b26aab8b61042af5bbeaf8fae5f1fcc3
Alisher.Kenzhebayev <alisher.kenzhebayev@nu.edu.kz>
2020-11-10 23:50:21 +0600

    Add basic U11_Linq no tests yet

---
+++ U11_Linq/DataLoader.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="DataLoader.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace U11_Linq
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Design;
+    using System.IO;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Text;
+    using U10_GenericsCollections.T_07;
+
+    public class DataLoader
+    {
+        private SimpleBst<StudentInfo> myBST;
+
+        // File structure should be like -> st.name, st.exam, st.date, st.score
+        public void LoadFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                var fileReader = new BinaryReader(File.Open(filePath, FileMode.Open));
+                while (fileReader.BaseStream.Position != fileReader.BaseStream.Length)
+                {
+                    var newBST = new StudentInfo
+                    {
+                        StudentName = fileReader.ReadString(),
+                        TestName = fileReader.ReadString(),
+                        TestDate = DateTime.Parse(fileReader.ReadString()),
+                        TestAssessment = double.Parse(fileReader.ReadString()),
+                    };
+
+                    this.myBST.Add(newBST);
+                }
+            }
+        }
+
+        public IEnumerable<StudentInfo> RequestData(Func<StudentInfo, bool> predicate = null)
+        {
+            predicate = predicate ?? (t => true);
+            var retVal = this.myBST.Where(predicate);
+            return retVal;
+        }
+    }
+}
---
+++ U11_Linq/StudentInfo.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="StudentInfo.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace U11_Linq
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    public class StudentInfo : IComparer, IComparable<StudentInfo>
+    {
+        public StudentInfo()
+        {
+            this.Comparable = null;
+        }
+
+        public StudentInfo(string studentName, string testName, DateTime testDate, double testAssessment, IComparer<StudentInfo> comparable = null)
+        {
+            this.StudentName = studentName;
+            this.TestName = testName;
+            this.TestDate = testDate;
+            this.TestAssessment = testAssessment;
+            this.Comparable = comparable;
+        }
+
+        public string StudentName { get; set; }
+
+        public string TestName { get; set; }
+
+        public DateTime TestDate { get; set; }
+
+        public double TestAssessment { get; set; }
+
+        public IComparer<StudentInfo> Comparable { get; set; }
+
+        public int Compare(object x, object y)
+        {
+            if (!(x is StudentInfo) || !(y is StudentInfo))
+            {
+                throw new ArgumentException("Objects are not instances of StudentInfo");
+            }
+
+            if (this.Comparable is null)
+            {
+                return x.ToString().CompareTo(y.ToString());
+            }
+            else
+            {
+                var vx = x as StudentInfo;
+                var vy = y as StudentInfo;
+                return this.Comparable.Compare(vx, vy);
+            }
+        }
+
+        public int CompareTo([AllowNull] StudentInfo other)
+        {
+            return this.Compare(this, other);
+        }
+    }
+}
